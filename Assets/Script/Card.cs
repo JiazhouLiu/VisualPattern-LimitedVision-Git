65066a7029c728b869ac932d54ed86e674c9b7d4
joeprincer <liujiazhou@gmail.com>
2019-12-17 22:57:56 +1100

    testing color for feedback

--- Assets/Script/Card.cs
+++ Assets/Script/Card.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
@@ -11,4 +12,41 @@ public class Card : MonoBehaviour
 
     public bool seen = false;
     public bool selected = false;
+
+    private Transform[] borders;
+
+    private void Start()
+    {
+        borders = new Transform[4]{
+            transform.GetChild(0).GetChild(0).GetChild(2),
+            transform.GetChild(0).GetChild(0).GetChild(3),
+            transform.GetChild(0).GetChild(0).GetChild(4),
+            transform.GetChild(0).GetChild(0).GetChild(5)
+        };
+    }
+
+    private void Update()
+    {
+        if (filled && seen && !selected) {
+            foreach (Transform t in borders) {
+                t.GetComponent<Image>().color = Color.yellow;
+            }
+        }
+
+        if (filled && !seen && selected)
+        {
+            foreach (Transform t in borders)
+            {
+                t.GetComponent<Image>().color = Color.blue;
+            }
+        }
+
+        if (filled && seen && selected)
+        {
+            foreach (Transform t in borders)
+            {
+                t.GetComponent<Image>().color = Color.green;
+            }
+        }
+    }
 }
