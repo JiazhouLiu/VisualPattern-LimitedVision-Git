ad0fb57d28afdc2a0c400f03552c5c1f5cabab36
joeprincer <liujiazhou@gmail.com>
2020-03-05 18:19:30 +1100

    changed calculate python file for analysis

--- Assets/ExperimentData/ExperimentLog/Participant 2/calculate.py
+++ Assets/ExperimentData/ExperimentLog/Participant 2/calculate.py
@@ -1,6 +1,15 @@
 import itertools
 import csv
+import os
 
+# Get participant number 
+cwd = os.getcwd()
+if cwd[-2] == " ":
+  pNum = cwd[-1]
+else:
+  pNum = cwd[-2] + cwd[-1]
+
+# Retrieve Manhattan Distance Error
 counter = 0  
 r = 12
 result = []
@@ -33,14 +42,41 @@ for aList in list(trialList):
       matrix[bList.index(tmpB)][aList.index(tmpA)] = (abs(int(a/r) - int(b/r)) + abs(a%r - b%r))
   result.append(min([sum([matrix[p[0]][p[1]] for p in enumerate(perm)]) for perm in itertools.permutations(range(len(matrix)))]))
 
-resultString = ""
+resultString = "Layout,Error\n"
 for a in list(result):
   if (counter % 2) == 0:
-    resultString += "Circle," + str(a) + "\n"
-  else:
     resultString += "Flat," + str(a) + "\n"
+  else:
+    resultString += "Circle," + str(a) + "\n"
   counter += 1
 
-f = open("result.csv", "w")
+f = open("result_" + pNum + "_ManhattanAccuracy.csv", "w")
+f.write(resultString)
+f.close()
+
+# Retrieve answer accuracy
+resultString = "Layout,Accuracy\n"
+with open('Participant_' + pNum + '_Answers.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader)
+    for row in csv_reader:
+        resultString += row[3] + "," + str(float(row[5]) / float(row[4])) + "\n"
+
+f = open("result_" + pNum + "_Accuracy.csv", "w")
+f.write(resultString)
+f.close()
+
+# Retrieve binary accuracy
+resultString = "Layout,Accuracy\n"
+with open('Participant_' + pNum + '_Answers.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader)
+    for row in csv_reader:
+        if float(row[5]) / float(row[4]) == 1:
+            resultString += row[3] + ",1\n"
+        else:
+            resultString += row[3] + ",0\n"
+
+f = open("result_" + pNum + "_BinaryAccuracy.csv", "w")
 f.write(resultString)
 f.close()
